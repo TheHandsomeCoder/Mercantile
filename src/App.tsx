463eb61c85b15cb916c6aad6ba28a95f7efde279
Scott O'Malley <scottyomalley@gmail.com>
2021-03-19 18:20:54 +0000

    Add additional filtering to the sample data

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Container, Grid } from 'semantic-ui-react';
 import InventoryTable from './components/InventoryTable';
 import inventory from './constant/items.json';
-const filteredInventory = inventory.item.filter(item => item.value && item.value <= 5000).map(i => ({ name: i.name, value: i.value, weight: i.weight }));
+const filteredInventory = inventory.item
+  .filter(item => item.value && item.value <= 5000)
+  .filter(item => item.source === 'PHB')
+  .map(i => ({ name: i.name, value: i.value, weight: i.weight }));
 
 const App: React.FC = () =>
   <Container fluid>
     <Grid divided={true}>
       <Grid.Row>
-        <Grid.Column width="8">          
+        <Grid.Column width="8">
         </Grid.Column>
         <Grid.Column width="8">
           <InventoryTable inventory={filteredInventory} />
