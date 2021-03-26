6022c8d7bc1df31bced237922958fa0b01bbbadb
Scott O'Malley <scottyomalley@gmail.com>
2021-03-26 00:17:06 +0000

    Add title bar and make shop name inline editable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Container, Grid, Form } from 'semantic-ui-react';
-import {InventoryTable, InventoryItemProps} from './components/InventoryTable';
+import React, { ChangeEvent, useState } from 'react';
+import { Container, Grid, Form, Menu, Input } from 'semantic-ui-react';
+import { InventoryTable, InventoryItemProps } from './components/InventoryTable';
 import inventory from './constant/items.json';
 
 const filteredInventory = (gp: number) => inventory.item
@@ -11,43 +11,57 @@ const filteredInventory = (gp: number) => inventory.item
 
 
 const App: React.FC = () => {
+  const [shopName, setShopName] = useState<string>('A Mercantile Enterprise');
+  const shopNameOnChange = (event: ChangeEvent) => {
+    const newValue = (event.target as HTMLInputElement).value;
+    setShopName(newValue);
+  };
+
   const [gp, setGp] = useState<number>(50);
   const gpOnChange = (event: Event) => {
-    const newValue = (event.target as HTMLInputElement).value;   
+    const newValue = (event.target as HTMLInputElement).value;
     setGp(Number(newValue));
   };
-  
+
   return (
-    <Container fluid className="main">
-      <Grid divided={true}>
-        <Grid.Row>
-          <Grid.Column width="8">
+    <>
+      <Menu>
+        <Menu.Item header>Mercantile</Menu.Item>
+      </Menu>
+      <Container fluid className="main">
+        <Grid divided={true}>
+          <Grid.Row>
+            <Grid.Column width="8">
 
-            <div className="add-margin-top">
-              <Grid padded className="entry-form">
-                <Grid.Row>
-                  <Grid.Column width="16">
-                    <h2>A mercantile enterprise</h2>
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                  <Grid.Column width="16">
-                    <Form>
-                      <Form.Field label='Items up to (GP)' control='input' type='number' onChange={gpOnChange} value={gp} />
-                    </Form>
-                </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </div>
+              <div className="add-margin-top">
+                <Grid padded className="entry-form">
+                  <Grid.Row>
+                    <Grid.Column width="16">
+                      <h2>
+                        <Input transparent fluid value={shopName} onChange={shopNameOnChange} />
+                      </h2>
+                      
+                    </Grid.Column>
+                  </Grid.Row>
+                  <Grid.Row>
+                    <Grid.Column width="16">
+                      <Form>
+                        <Form.Field label='Items up to (GP)' control='input' type='number' onChange={gpOnChange} value={gp} />
+                      </Form>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </div>
 
-          </Grid.Column>
-          <Grid.Column width="8">
-            <InventoryTable inventory={filteredInventory(gp)} />
-          </Grid.Column>
-        </Grid.Row>
+            </Grid.Column>
+            <Grid.Column width="8">
+              <InventoryTable inventory={filteredInventory(gp)} />
+            </Grid.Column>
+          </Grid.Row>
 
-      </Grid>
-    </Container>);
+        </Grid>
+      </Container>
+    </>);
 }
 
 
