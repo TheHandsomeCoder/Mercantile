c3582c2ef56b309bc3eab2700c91922bf2e35d29
Scott O'Malley <scottyomalley@gmail.com>
2021-03-30 10:02:54 +0100

    Update extract sourcebooks script and computed data

--- src/scripts/extract-sourcebooks.js
+++ src/scripts/extract-sourcebooks.js
@@ -2,7 +2,7 @@ require("../static/parser.js");
 const inventory = require('../constant/items.json');
 const fs = require('fs');
 
-const handbookTLAToFullText = inventory.item.reduce((acc,cur) => acc.set(cur.source, Parser.sourceJsonToFull(cur.source)), new Map())
+const handbookTLAToFullText = inventory.item.reduce((acc,cur) => cur.value ? acc.set(cur.source, Parser.sourceJsonToFull(cur.source)) : acc, new Map())
 
 
 fs.writeFileSync('./src/computed/sourcebooks.json', JSON.stringify(Object.fromEntries(handbookTLAToFullText.entries())))
