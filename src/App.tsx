0cd9ef9b8ec1d71c7202cebc9c60e94c2ee0ed0f
Scott O'Malley <scottyomalley@gmail.com>
2021-04-05 16:34:15 +0100

    Initial integration for PHB

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,42 @@ import { Container, Grid, Form, Menu } from 'semantic-ui-react';
 import { EditableHeader } from './components/EditableHeader';
 import { InventoryTable, InventoryItemProps } from './components/InventoryTable';
 import inventory from './computed/items.json';
-import sourcebooks from './computed/sourcebooks.json';
 import itemTypes from './computed/itemTypes.json'
+import sourcebooks from './computed/sourcebooks.json';
 
 const capitalizeFirstLetter = (s: string) => {
   return s[0].toUpperCase() + s.slice(1);
 }
 
-const filteredInventory = (gp: number, sourcebook: string, type: string) => {
-  return inventory
-    .filter(item => item.value !== undefined)
-    .filter(item => (!sourcebook) ? item : item.source === sourcebook)
-    .filter(item => item.value && item.value <= gp * 100)
-    .filter(item => item.source === sourcebook)
-    .filter(item => item.type === type)
-    .map(i => ({ name: i.name, value: i.value, weight: i.weight } as InventoryItemProps));
-}
+// const filteredInventory = (gp: number, sourcebook: string, type: string) => {
+//   return inventory
+//     .filter(item => item.value !== undefined)
+//     .filter(item => (!sourcebook) ? item : item.source === sourcebook)
+//     .filter(item => item.value && item.value <= gp * 100)
+//     .filter(item => item.source === sourcebook)
+//     .filter(item => item.type === type)
+//     .map(i => ({ name: i.name, value: i.value, weight: i.weight } as InventoryItemProps));
+// }
 
-const sourcebooksAsOptions = Object.entries(sourcebooks).sort().map(([key, value]) => ({ key, text: value, value: key }));
+// const sourcebooksAsOptions = Object.entries(sourcebooks).sort().map(([key, value]) => ({ key, text: value, value: key }));
+// const sourcebooksAsOptions = [{ key, 'PHB': "Player's Handbook", value: 'PHB' }];
 //TODO: Move the capitalization to precompute
-const itemTypesAsOptions = Object.entries(itemTypes).sort().map(([key, value]) => ({ key, text: capitalizeFirstLetter(value), value: key }));
+// const itemTypesAsOptions = Object.entries(itemTypes).sort().map(([key, value]) => ({ key, text: capitalizeFirstLetter(value), value: key }));
+const itemTypesAsOptions = [
+  { key: 'AG', text: capitalizeFirstLetter('Adventuring Gear'), value: 'AG', as: "Dropdown.Divider", disabled: true},
+  { key: 'A', text: capitalizeFirstLetter('Ammunition'), value: 'A' },
+  { key: 'SCF-druid', text: capitalizeFirstLetter('Druidic Focus'), value: 'SCF-druid' },
+  { key: 'SCF-arcane', text: capitalizeFirstLetter('Arcane Focus'), value: 'SCF-arcane' },
+  { key: 'SCF-holy', text: capitalizeFirstLetter('Hold Focus'), value: 'SCF-holy' }
+]
+
+
 
 const App: React.FC = () => {
-  const [sourceBook, setSourceBook] = useState<string>('PHB');
-  const sourceBookOnChange = (event: SyntheticEvent, data: any) => {
-    setSourceBook(data.value);
-  };
+  // const [sourceBook, setSourceBook] = useState<string>('PHB');
+  // const sourceBookOnChange = (event: SyntheticEvent, data: any) => {
+  //   setSourceBook(data.value);
+  // };
 
   const [shopName, setShopName] = useState<string>('A Mercantile Enterprise');
   const shopNameOnChange = (event: ChangeEvent) => {
@@ -42,7 +52,7 @@ const App: React.FC = () => {
     setGp(Number(newValue));
   };
 
-  const [itemType, setItemType] = useState<string>('G');
+  const [itemType, setItemType] = useState<string[]>([]);
   const itemTypeOnChange = (event: SyntheticEvent, data: any) => {
     setItemType(data.value);
   };
@@ -69,14 +79,6 @@ const App: React.FC = () => {
                   <Grid.Row>
                     <Grid.Column width="16">
                       <Form>
-                        <Form.Select
-                          label="Sourcebook"
-                          search
-                          selection
-                          onChange={sourceBookOnChange}
-                          value={sourceBook}
-                          options={sourcebooksAsOptions}
-                        />
                         <Form.Field
                           label="Items up to (GP)"
                           control="input"
@@ -88,6 +90,7 @@ const App: React.FC = () => {
                           label="Item Type"
                           search
                           selection
+                          multiple
                           onChange={itemTypeOnChange}
                           value={itemType}
                           options={itemTypesAsOptions}
@@ -99,7 +102,7 @@ const App: React.FC = () => {
               </div>
             </Grid.Column>
             <Grid.Column width="8">
-              <InventoryTable inventory={filteredInventory(gp, sourceBook, itemType)} />
+              {/* <InventoryTable inventory={filteredInventory(gp, sourceBook, itemType)} /> */}
             </Grid.Column>
           </Grid.Row>
         </Grid>
